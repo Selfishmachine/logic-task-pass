6976ea7527d17cf340d3ee4b3d45a9bc95e846c5
haider sadoon <91218301+Selfishmachine@users.noreply.github.com>
2022-08-14 22:53:24 +0300

    Update Q1.py

--- Logic pass/Q1.py
+++ Logic pass/Q1.py
@@ -1,7 +1,8 @@
-word = input("Enter your sentence: ")
-rw = input("Enter any thing you want to remove: ")
+class remove():
+   word = input("Enter your sentence: ")
+   rw = input("Enter any thing you want to remove: ")
 
-print("\n Your sentence: ", word)
-word = word.replace(rw, "")
+   print("\n Your sentence: ", word)
+   word = word.replace(rw, "")
 
-print(" Your sentence after removing letter(s): ", word)
+   print(" Your sentence after removing letter(s): ", word)
